087dac90e4013a2a69c0607f9ff8bf6106fdae2c
Hiroyuki Nakazato <nakazato.hiroyuki@gmail.com>
2024-03-14 02:59:19 +0900

    Change dir name and outputs

---
+++ examples/multi_mws/modules/aws_databricks_mws/outputs.tf
@@ -0,0 +1,8 @@
+output "databricks_workspace_url" {
+  value = databricks_mws_workspaces.this.workspace_url
+}
+
+output "databricks_workspace_token" {
+  value     = databricks_mws_workspaces.this.token[0].token_value
+  sensitive = true
+}
