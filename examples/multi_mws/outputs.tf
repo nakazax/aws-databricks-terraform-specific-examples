3c8ffe46f915337240eab3763538c7379b0c1e83
Hiroyuki Nakazato <nakazato.hiroyuki@gmail.com>
2024-03-14 03:02:27 +0900

    Modify attr name of outputs

--- examples/multi_mws/outputs.tf
+++ examples/multi_mws/outputs.tf
@@ -1,7 +1,7 @@
 output "databricks_workspaces_details" {
   value = [for key, ws in module.aws_databricks_mws : {
-    url   = ws.databricks_workspace_url
-    token = ws.databricks_workspace_token
+    databricks_workspace_url   = ws.databricks_workspace_url
+    databricks_workspace_token = ws.databricks_workspace_token
   }]
   sensitive = true
 }
